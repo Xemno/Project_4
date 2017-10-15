6ad3ae9fa79495720e92f9e2f3e7380ec47d2b5a
Cinderella25 <cinderella25.cindy25@gmail.com>
2017-10-15 16:41:51 +0200

    XmlSensor.java und SoapActivity.java verändert (15.10.17)

--- app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/SoapActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/SoapActivity.java
@@ -3,11 +3,24 @@ package ch.ethz.inf.vs.a2.ankoller.webservices;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SoapActivity extends AppCompatActivity {
+import ch.ethz.inf.vs.a2.ankoller.webservices.sensor.SensorListener;
+
+public class SoapActivity extends AppCompatActivity implements SensorListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soap2);
+    }
+
+
+    @Override
+    public void onReceiveSensorValue(double value){
+
+    }
+
+    @Override
+    public void onReceiveMessage(String message) {
+
     }
 }
--- app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/sensor/XmlSensor.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/sensor/XmlSensor.java
@@ -1,20 +1,104 @@
 package ch.ethz.inf.vs.a2.ankoller.webservices.sensor;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by anja on 13.10.2017.
  */
 
-class XmlSensor implements Sensor {
+
+
+class XmlSensor extends AbstractSensor {
+
+
     @Override
     public String executeRequest() throws Exception {
+
+        String server = "http://vslab.inf.ethz.ch:8080/SunSPOTWebServices/SunSPOTWebservice?WSDL";
+        URL url = new URL(server);
+        //Casting what the openConnection() returns.
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+
+        String requestString = "<?xml version='1.0' encoding='UTF-8'?><S:Envelope xmlns:S='http://schemas.xmlsoap.org/soap/envelope/'>";
+        requestString += "<S:Header/>";
+        requestString += "<S:Body>";
+        requestString += "<ns2:getSpot xmlns:ns2='http://webservices.vslecture.vs.inf.ethz.ch/'>";
+        requestString += "<id>Spot3</id>";
+        requestString += "</ns2:getSpot>";
+        requestString += "</S:Body>";
+        requestString += "</S:Envelope>";
+
+        byte[] reqStrBytes = requestString.getBytes("UTF-8");
+
+        connection.setRequestProperty("Content-Type", "text/xml");
+        connection.setRequestProperty("Content-Length", String.valueOf(reqStrBytes.length));
+        connection.getOutputStream().write(reqStrBytes);
+
+        int responseCode = connection.getResponseCode();
+        //Log.d("LOG", String.valueOf(responseCode));
+        if (responseCode == 200) {
+
+            BufferedReader responseReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String responseLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((responseLine = responseReader.readLine()) != null) {
+                response.append(responseLine);
+                response.append("\n");
+            }
+            responseReader.close();
+            //Log.d("LOG", response.toString());
+            return response.toString();
+        }
+
         return null;
     }
 
     @Override
+    //From: https://developer.android.com/reference/org/xmlpull/v1/XmlPullParser.html
     public double parseResponse(String response) {
-        return 0;
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            XmlPullParser xpp = factory.newPullParser();
+
+            xpp.setInput( new StringReader(response) );
+
+            int eventType = xpp.getEventType();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if(eventType == XmlPullParser.START_TAG) {
+                    if (xpp.getName().equals("temperature")) {
+                        xpp.next();
+                        if (xpp.getEventType() == XmlPullParser.TEXT) {
+                            return Double.valueOf(xpp.getText());
+                        }
+                    }
+                }
+                    xpp.next();
+            }
+
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+
+        return Double.NaN;
     }
 
+    /**
     @Override
     public void getTemperature() {
 
@@ -29,4 +113,5 @@ class XmlSensor implements Sensor {
     public void unregisterListener(SensorListener listener) {
 
     }
+    */
 }
