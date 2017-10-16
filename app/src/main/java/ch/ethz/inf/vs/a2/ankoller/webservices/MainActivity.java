64b6d5d77e72c40ed752f8cbbae584d9653e8227
Qais <qaise@student.ethz.ch>
2017-10-16 13:46:34 +0200

    Merge branch 'master' of https://github.com/Xemno/Project_4
    # Conflicts:
    #	.idea/modules.xml

--- app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/MainActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void onButtonClick(View v) {
         Intent intent;
         switch (v.getId()) {
