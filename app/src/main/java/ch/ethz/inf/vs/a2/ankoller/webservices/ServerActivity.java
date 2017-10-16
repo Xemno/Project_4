73771e61113bdc78114dec5b51da0dedc56ff426
Qais <qaise@student.ethz.ch>
2017-10-16 16:55:33 +0200

    Part 3 - Phone as a Server / Qais / Commit Version Nr. 1.1

--- app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/ServerActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/ServerActivity.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,9 +20,14 @@ import java.util.Enumeration;
 public class ServerActivity extends AppCompatActivity {
 
     private static final String TAG = "Server Activity: ";
+    private static final String BUTTON_STATE = "ch.ethz.inf.vs.a2.ankoller.webservices.Button";
+    private static final String IP_STATE = "ch.ethz.inf.vs.a2.ankoller.webservices.IP";
+    private static final String PORT_STATE = "ch.ethz.inf.vs.a2.ankoller.webservices.PORT";
+    private static final String DEFAULT_PORT = "1234";
+    private static final String DEFAULT_IP = "0.0.0.0";
 
     private ToggleButton button_Server;
-    private TextView tv_port, tv_server;
+    private TextView tv_port, tv_ip;
 
     private localBroadcastReceiver lbr; // broadcast receiver
 
@@ -36,7 +42,7 @@ public class ServerActivity extends AppCompatActivity {
 
         button_Server = (ToggleButton) findViewById(R.id.toggleButton);
         tv_port = (TextView) findViewById(R.id.port_address_view);
-        tv_server = (TextView) findViewById(R.id.ip_address_view);
+        tv_ip = (TextView) findViewById(R.id.ip_address_view);
 
 
         /* Now we show the Network Interfaces */
@@ -56,21 +62,45 @@ public class ServerActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Save settings
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = sharedPreferences.edit();
+        editor.putBoolean(BUTTON_STATE, button_Server.isChecked());
+        editor.putString(IP_STATE, tv_ip.getText().toString());
+        editor.putString(PORT_STATE, tv_port.getText().toString());
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Retrieve settings
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        button_Server.setChecked(sharedPreferences.getBoolean(BUTTON_STATE, false));
+        tv_ip.setText(sharedPreferences.getString(IP_STATE, DEFAULT_IP));
+        tv_port.setText(sharedPreferences.getString(PORT_STATE, DEFAULT_PORT));
+    }
+
     public class localBroadcastReceiver extends BroadcastReceiver {
         public void onReceive(Context context, Intent intent) {
             tv_port.setText("" + intent.getIntExtra("PORT", 1234)); // default value 1234
-            tv_server.setText(intent.getStringExtra("IP"));
+            tv_ip.setText(intent.getStringExtra("IP"));
         }
     }
 
-    public void clicked_tbServer(View view){
+    public void onButtonClick (View view){
         if (button_Server.isChecked()){
             startService(new Intent(this, Server.class));
         }
         else {
             stopService(new Intent(this, Server.class));
-            tv_port.setText("0.0.0.0");
-            tv_server.setText("1234");
+            tv_port.setText(DEFAULT_IP);
+            tv_ip.setText(DEFAULT_PORT);
         }
     }
 }
