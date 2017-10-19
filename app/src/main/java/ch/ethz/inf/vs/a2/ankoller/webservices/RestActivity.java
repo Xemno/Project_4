de35ce064c65d56894a35ea51fa4eb69028964ba
anjakoller <32456749+anjakoller@users.noreply.github.com>
2017-10-19 11:41:07 +0200

    Anja commit 19.10. 11:40 Uhr

--- app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/RestActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/RestActivity.java
@@ -5,23 +5,48 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import ch.ethz.inf.vs.a2.ankoller.webservices.sensor.AbstractSensor;
+import ch.ethz.inf.vs.a2.ankoller.webservices.sensor.RawHttpSensor;
 import ch.ethz.inf.vs.a2.ankoller.webservices.sensor.SensorListener;
 
 public class RestActivity extends AppCompatActivity implements SensorListener{
     //TODO implement methods, this activity has to implement the interface sensorlistener
     // so that activity can be informed upon reception of the temperature value
+    //first create a new RawHttpSensor object, register your activity as a listener
+    //and invoke a request to retrieve a new temperature value
+    //display the retrieved temperature value
+    RawHttpSensor rawHttpSensor;
+    AbstractSensor sensor;
+    TextView displayValue;
+    String celsius;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rest);
+       displayValue=(TextView)findViewById(R.id.displayvalue_view);
+        displayValue.setText("wait to get temperature value:");
+        celsius=" °C";
+
+
+        rawHttpSensor= new RawHttpSensor();
+        sensor=rawHttpSensor;
+
+        //registerlistener and get temp value
+        sensor.registerListener(this);
+        sensor.getTemperature();
+
 
 
     }
 
     @Override
     public void onReceiveSensorValue(double value) {
+        //display the sensor value
+
+    String text= displayValue + celsius;
+        displayValue.setText(text);
 
     }
 
--- app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/sensor/TextSensor.java
+++ app/src/main/java/ch/ethz/inf/vs/a2/ankoller/webservices/sensor/TextSensor.java
@@ -1,13 +1,32 @@
 package ch.ethz.inf.vs.a2.ankoller.webservices.sensor;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static ch.ethz.inf.vs.a2.ankoller.webservices.http.RemoteServerConfiguration.HOST;
+import static ch.ethz.inf.vs.a2.ankoller.webservices.http.RemoteServerConfiguration.REST_PORT;
+
 /**
  * Created by anja on 13.10.2017.
  */
 
-class TextSensor implements Sensor {
+class TextSensor extends AbstractSensor implements Sensor {
+//implement missing methods
+    //create requests by invoking openConnection() on a java.net.URL object
+    //set header fields with setRequestProperty(header,value) on the
+    //HttpURLConnection object
+private static final String SENSOR_PATH = "/sunspots/Spot1/sensors/temperature";
     @Override
     public String executeRequest() throws Exception {
-        return null;
+        //create a URL
+        URL url= new URL("http://"+ HOST+ ":" + REST_PORT + SENSOR_PATH);
+        //create a HTTPURLConnection object
+        HttpURLConnection urlConnection= (HttpURLConnection)url.openConnection();
+        //set header fields
+        urlConnection.setRequestProperty("Accept", "text/plain");
+       String response= "";
+      //TODO inputstream add and separate with \n and at end disconnect
+        return response;
     }
 
     @Override
